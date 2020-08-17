70467108ce52633b5685a975f8391fd447d22c3b
fountainstudios <51059688+fountainstudios@users.noreply.github.com>
2020-08-17 12:14:38 +0200

    Update pxt.json, test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,36 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+// datasets
+datasets.prepare()
+datasets.dataset3()
+datasets.dataset2()
+datasets.dataset1()
+datasets.data3()
+datasets.data2()
+datasets.data1()
+datasets.onEvent(datasetType.historical, function() {
+})
+
+// Input
+Input.videoFeed()
+Input.distanceSensor()
+Input.waterquality()
+Input.groundSensor()
+Input.weatherSatelite()
+Input.Arialphotos()
+Input.photos()
+
+//AI
+AI.plantingLocation()
+AI.differences()
+AI.pollution()
+AI.train()
+AI.compare2(0)
+AI.compare1(0, 0)
+AI.goal()
+AI.action(actionML.classify)
+AI.compare(datasetType.historical, datasetType.historical)
+AI.onEvent2(ML.supervised, datasetType.historical, function() {
+})
+AI.onEvent1(ML.supervised, function() {
+})
+AI.onEvent(ML.supervised, function() {
+})
