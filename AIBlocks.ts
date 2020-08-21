b104b501d11a91a0e6fece2f5d4439426c5b9b33
fountainstudios <51059688+fountainstudios@users.noreply.github.com>
2020-08-21 11:33:29 +0200

    Update pxt.json, AIBlocks.ts

--- AIBlocks.ts
+++ AIBlocks.ts
@@ -25,56 +25,9 @@ enum actionML {
     getPrediction
 }
 
-//% color="#4C97FF"
-namespace datasets {
-
-    //% block="%type| dataset"
-    export function onEvent(type: datasetType, handler: () => void) {
-        if (type == 1){
-            blocks.place(GRASS, pos(1,0,0));
-        }else{
-            blocks.place(GRASS, pos(2,0,0));
-        }
-    }
-
-    //% block="gather data"
-    export function data1() {
-    /*Random Code*/
-    }
-
-    //% block="upload data"
-    export function data2() {
-    /*Random Code*/
-    }
-
-    //% block="label data"
-    export function data3() {
-    /*Random Code*/
-    }
-
-    //% block="historical dataset"
-    export function dataset1(): number {
-        return 0;
-    }
-
-    //% block="current dataset"
-    export function dataset2(): number {
-        return 0;
-    }
-
-    //% block="live dataset"
-    export function dataset3(): number {
-        return 0;
-    }
-
-    //% block="prepare data"
-    export function prepare() {
-        blocks.place(GRASS, pos(1,1,1));
-    }
-}
 //% color="#8c800b"
 namespace Input{
-        //% block="input photos"
+    //% block="input photos"
     export function photos(){
     }
 
@@ -103,36 +56,46 @@ namespace Input{
     }
 }
 
+//% color="#4C97FF"
+namespace datasets {
+
+    //% block="%type| dataset"
+    export function onEvent(type: datasetType, handler: () => void) {
+        if (type == 1){
+            blocks.place(GRASS, pos(1,0,0));
+        }else{
+            blocks.place(GRASS, pos(2,0,0));
+        }
+    }
+
+    //% block="label data"
+    export function data3() {
+    /*Random Code*/
+    }
+
+    //% block="historical dataset"
+    export function dataset1(): number {
+        return 0;
+    }
+
+    //% block="current dataset"
+    export function dataset2(): number {
+        return 0;
+    }
+
+    //% block="live dataset"
+    export function dataset3(): number {
+        return 0;
+    }
+
+}
+
+
 //% color="#8332A8"
 namespace AI {
 
-    //% block="%type| machine learning"
-    export function onEvent(type: ML, handler: () => void) {
-    /*Random Code*/
-    }
-
-    //% block="%type| machine learning training"
-    export function onEvent1(type: ML, handler: () => void) {
-    /*Random Code*/
-    }
-
-    //% block="%type| machine learning algorithm with %datatype| dataset"
-    export function onEvent2(type: ML, datatype: datasetType, handler: () => void) {
-    /*Random Code*/
-    }
-
-    //% block="compare %dataset| against %dataset|"
-    export function compare(dataset: datasetType, datasetType1: datasetType) {
-
-    }
-
-    //% block="find pattern and %MLaction|"
-    export function action(MLaction: actionML) {
-    /*Random Code*/
-    }
-
-    //% block="set goal to reach broken sensor"
-    export function goal() {
+    //% block="machine learning"
+    export function onEvent3(handler: () => void) {
     /*Random Code*/
     }
 
@@ -145,12 +108,12 @@ namespace AI {
     export function compare2(value:number) {
     /*Random Code*/
     }
-    
-    //% block="train AI"
-    export function train() {
-    /*Random Code*/
-    }
 
+}
+
+//% color="#2d854e"
+namespace Goals {
+    
     //% block="find pollution location"
     export function pollution() {
     /*Random Code*/
@@ -165,6 +128,19 @@ namespace AI {
     export function plantingLocation() {
     /*Random Code*/
     }
+    
+     //% block="find pattern and classify|"
+    export function classify() {
+    /*Random Code*/
+    }
 
+    //% block="find pattern and get prediction|"
+    export function prediction() {
+    /*Random Code*/
+    }
 
+    //% block="reach broken sensor"
+    export function goal() {
+    /*Random Code*/
+    }
 }
