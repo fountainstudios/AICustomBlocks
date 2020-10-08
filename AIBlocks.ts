98d59c354d02095de3fecdd131a54346f63b4bde
fountainstudios <51059688+fountainstudios@users.noreply.github.com>
2020-10-08 16:03:30 +0200

    Update AIBlocks.ts

--- AIBlocks.ts
+++ AIBlocks.ts
@@ -2,9 +2,9 @@ enum datasetType {
     //% block="historical"
     historical = 0,
     //% block="current"
-    current,
+    current = 1,
     //% block="live"
-    live
+    live = 2
 }
 
 enum ML {
@@ -26,9 +26,19 @@ namespace Input{
         blocks.place(STONE, world(96, 61, -397));
     }
 
+    //% block="location 1 picture data"
+    export function location1PictureData_MTB() {
+        agent.move(FORWARD, 2)
+    }
+
     //% block="location 2 picture data"
     export function location2PictureData_MT() {
         blocks.place(STONE, world(96, 61, -396));
+    }
+
+    //% block="location 2 picture data"
+    export function location2PictureData_MTB() {
+        agent.move(RIGHT, 2)
     }
 
     //% block="location 3 picture data"
@@ -36,12 +46,14 @@ namespace Input{
         blocks.place(STONE, world(96, 61, -395));
     }
 
+    //% block="location 3 picture data"
+    export function location3PictureData_MTB() {
+        agent.move(FORWARD, 2)
+    }
+
     //% block="live sensor 1 data"
     export function sensor1_OO() {
-        `blocks.place(STONE, world(1, 4, 1));`
-        agent.move(FORWARD, 2)
-        agent.turn(TurnDirection.Left)
-        agent.move(FORWARD, 2)
+        blocks.place(STONE, world(1, 4, 1))
     }
 
     //% block="live sensor 2 data"
@@ -71,17 +83,20 @@ namespace Datasets{
         handler();
     }
 
+    //% block="make dataset"
+    //% handlerStatement=1
+    export function setOfData_MTB(handler: () => void) {
+        let i = 0
+        for(i = 0; i<2; i++){
+            handler();
+        }
+    }
+
     //% block="make %type| dataset"
     //% handlerStatement=1
     export function setOfData_OO(type: datasetType, handler: () => void) {
-        `
         if(type == 2){
             blocks.place(STONE, world(0, 4, 1));
-            handler();
-        }
-        `
-        let i = 0
-        for(i = 0; i<2; i++){
             handler();
         }
     }
@@ -93,12 +108,13 @@ namespace Datasets{
 
     //%block="historical dataset"
     export function historicalData():number{
-        return 1
+        return 0
     }
 
     //%block="live dataset"
     export function liveData():number{
-        return 2
+        let i = 2
+        return i
     }
 
     //% block="input dataset"
@@ -106,21 +122,40 @@ namespace Datasets{
     blocks.place(STONE, world(110, 61, -397));
     }
 
+    //% block="input dataset"
+    export function input_MTB() {
+    agent.move(FORWARD, 2)
+    }
+
 }
 
 //% color="#8332A8"
 namespace AI {
-    
+
     //% block="unsupervised machine learning"
     //% handlerStatement=1
     export function ml_MT(handler: () => void) {
         blocks.place(STONE, world(110, 61, -398));
         handler();
     }
+
+    //% block="unsupervised machine learning"
+    //% handlerStatement=1
+    export function ml_MTB(handler: () => void) {
+        let i = 0
+        for(i = 0; i<2; i++){
+            handler();
+        }
+    }
     
     //% block="analyze data"
     export function analyze_MT(){
         blocks.place(STONE, world(110, 61, -396));
+    }
+
+    //% block="analyze data"
+    export function analyze_MTB(){
+        agent.move(LEFT, 2)
     }
 
     //% block="machine learning"
@@ -137,7 +172,7 @@ namespace AI {
 
     //% block="input %blockType|"
     export function input_OO(blockType:number){
-        if(blockType == 2){
+        if(blockType == 0){
            blocks.place(STONE, world(10, 12, 10))  
         }
     }
@@ -190,6 +225,11 @@ namespace Output {
     export function showLocations_MT(){
         blocks.place(STONE, world(110, 61, -395));
     }
+
+    //% block="show locations"
+    export function showLocations_MTB(){
+        agent.move(FORWARD, 2)
+    }
 }
 
 //% color="#ED82B6"
@@ -199,8 +239,6 @@ namespace Test{
     //% handlerStatement=1
     export function testLoop(handler: () => void) {
         let i = 0
-        agent.teleport(world(103, 168, -393), WEST)
-    
         for(i = 0; i<2; i++){
         handler();
         }
