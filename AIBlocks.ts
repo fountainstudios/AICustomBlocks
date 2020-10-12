c22454a09ba95b62056b1f9f94327c0c367d07e7
fountainstudios <51059688+fountainstudios@users.noreply.github.com>
2020-10-12 17:51:17 +0200

    Update AIBlocks.ts

--- AIBlocks.ts
+++ AIBlocks.ts
@@ -71,6 +71,21 @@ namespace Input{
         blocks.place(STONE, world(1, 4, 4));
     }
 
+    //% block="trap camera 1 data"
+    export function trap1_PR() {
+        agent.move(FORWARD, 2)
+    }
+
+    //% block="trap camera 2 data"
+    export function trap2_PR() {
+        agent.move(RIGHT, 2)
+    }
+
+    //% block="trap camera 3 data"
+    export function trap3_PR() {
+        agent.move(LEFT, 3)
+    }
+
 }
 
 //% color="#4C97FF"
@@ -96,9 +111,27 @@ namespace Datasets{
     //% handlerStatement=1
     export function setOfData_OO(type: datasetType, handler: () => void) {
         if(type == 2){
-            blocks.place(STONE, world(0, 4, 1));
+            let i = 0
+            for(i = 0; i<2; i++){
             handler();
+            }
         }
+    }
+
+    //% block="make %type| dataset"
+    //% handlerStatement=1
+    export function setOfData_PR(type: datasetType, handler: () => void) {
+        if(type == 0){
+            let i = 0
+            for(i = 0; i<2; i++){
+            handler();
+            }
+        }
+    }
+
+    //% block="extract patterns"
+    export function extractPatterns_PR() {
+        agent.move(FORWARD, 4)
     }
 
     //%block="current dataset"
@@ -127,6 +160,19 @@ namespace Datasets{
     agent.move(FORWARD, 2)
     }
 
+    //% block="input datasets"
+    export function input_PR() {
+    agent.move(RIGHT, 2)
+    }
+
+    //% block="compare %type0| dataset against %type1|"
+    //% handlerStatement=1
+    export function compare_PR(type0: datasetType, type1: datasetType) {
+        if(type0==0 && type1==1){
+            agent.move(FORWARD, 2)
+        }
+    }
+
 }
 
 //% color="#8332A8"
@@ -142,6 +188,15 @@ namespace AI {
     //% block="unsupervised machine learning"
     //% handlerStatement=1
     export function ml_MTB(handler: () => void) {
+        let i = 0
+        for(i = 0; i<2; i++){
+            handler();
+        }
+    }
+
+    //% block="semi-supervised machine learning"
+    //% handlerStatement=1
+    export function ml_PR(handler: () => void) {
         let i = 0
         for(i = 0; i<2; i++){
             handler();
@@ -180,6 +235,16 @@ namespace AI {
     //% block="generate routes"
     export function genRoutes_OO(){
         blocks.place(STONE, world(10, 13, 10))  
+    }
+
+    //% block="locate ocelots"
+    export function locateOcelots_PR(){
+        agent.move(RIGHT, 4)
+    }
+
+    //% block="analyze pattern data"
+    export function analyze_PR(){
+        agent.move(LEFT, 3)
     }
 
 }
@@ -229,6 +294,16 @@ namespace Output {
     //% block="show locations"
     export function showLocations_MTB(){
         agent.move(RIGHT, 2)
+    }
+
+    //% block="show locations"
+    export function showLocations_PR(){
+        agent.move(BACK, 3)
+    }
+
+    //% block="genrate migration routes"
+    export function genRoutes_PR(){
+        agent.move(FORWARD, 4)
     }
 }
 
